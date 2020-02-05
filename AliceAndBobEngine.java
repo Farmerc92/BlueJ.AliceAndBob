eadb6c43797773f74b298977234be2e370047a40
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-05 13:08:13 -0500

    completed the evaluateAliceOrBob method

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -1,9 +1,10 @@
 
 /**
- * Write a description of class AliceAndBobEngine here.
+ * Evaluates if the String is "Bob" or "Alice" are the input.  In that case it concatinates with a "Hello ".  Also evaluate if the String "Bobert" or 
+ * "Alicia" are the input.  In that case the output is "I DON'T KNOW YOU, YOU ARE SCARY".  
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Chris Farmer
+ * @version Version 1.1
  */
 public class AliceAndBobEngine
 {
@@ -16,7 +17,12 @@ public class AliceAndBobEngine
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
+        if (input.equals("Alice") || input.equals("Bob")){
+            return "Hello " + input;
+        }
+        else if (input.equals ("Bobert") || input.equals("Alicia")){
+            return "I DON'T KNOW YOU, YOU ARE SCARY";
+        }
         return result;
     }
 }
